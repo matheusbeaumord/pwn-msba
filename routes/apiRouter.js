b20041468a2a823da576e0ce09dcf28766c86bc7
Matheus <matheusbeaumord@gmail.com>
2022-08-06 19:21:17 -0300

    feat: Heroku DB comunication

---
+++ routes/apiRouter.js
@@ -0,0 +1,34 @@
+const express = require('express');
+let apiRouter = express.Router();
+const endpoint = '/';
+
+const knex = require('knex')({
+  client: 'pg',
+  debug: true,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+});
+
+const lista_produtos = {
+  produtos: [
+    { id: 1, descricao: 'Produto 1', valor: 5.0, marca: 'marca ' },
+    { id: 2, descricao: 'Produto 2', valor: 5.0, marca: 'marca ' },
+    { id: 3, descricao: 'Produto 3', valor: 5.0, marca: 'marca ' },
+  ],
+};
+
+apiRouter.get(endpoint + 'produtos', (req, res) => {
+  knex
+    .select('*')
+    .from('produto')
+    .then((produtos) => res.status(200).json(produtos))
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Erro ao recuperar produtos - ' + err.message,
+      });
+    });
+});
+
+module.exports = apiRouter;
