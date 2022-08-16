8ab67465b245e255edacb47df13e04fb15b54e77
Matheus <matheusbeaumord@gmail.com>
2022-08-16 11:32:40 -0300

    feat: jwt configuration

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 let apiRouter = express.Router();
 const endpoint = '/';
 
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
 const knex = require('knex')({
   client: 'pg',
   debug: true,
@@ -11,8 +15,51 @@ const knex = require('knex')({
   },
 });
 
+//Authentication middlewware
+const checkToken = (req, res, next) => {
+  let authToken = req.headers['authorization'];
+  if (!authToken) {
+    res.status(401).json({ message: 'Token de acesso requerida' });
+  } else {
+    let token = authToken.split(' ')[1];
+    req.token = token;
+  }
+  jwt.verify(req.token, process.env.SECRET_KEY, (err, decodeToken) => {
+    if (err) {
+      res.status(401).json({ message: 'Acesso negado' });
+      return;
+    }
+  });
+};
+
+const isAdmin = (req, res, next) => {
+  knex
+    .select('*')
+    .from('usuario')
+    .where({ id: req.usuarioId })
+    .then((usuarios) => {
+      if (usuarios.length) {
+        let usuario = usuarios[0];
+        let roles = usuario.roles.split(';');
+        let adminRole = roles.find((i) => i === 'ADMIN');
+        if (adminRole === 'ADMIN') {
+          next();
+          return;
+        } else {
+          res.status(403).json({ message: 'Role de ADMIN requerida' });
+          return;
+        }
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Erro ao verificar roles de usuário - ' + err.message,
+      });
+    });
+};
+
 // Request all foods
-apiRouter.get(endpoint + 'food', (req, res) => {
+apiRouter.get(endpoint + 'food', checkToken, (req, res) => {
   knex
     .select('*')
     .from('food')
@@ -25,7 +72,7 @@ apiRouter.get(endpoint + 'food', (req, res) => {
 });
 
 // Request a food by id
-apiRouter.get(endpoint + 'food/:id', (req, res) => {
+apiRouter.get(endpoint + 'food/:id', checkToken, (req, res) => {
   let id = parseInt(req.params.id);
 
   knex
@@ -41,7 +88,7 @@ apiRouter.get(endpoint + 'food/:id', (req, res) => {
 });
 
 // Add a new food
-apiRouter.post(endpoint + 'food', (req, res) => {
+apiRouter.post(endpoint + 'food', checkToken, isAdmin, (req, res) => {
   knex('food')
     .insert(
       {
@@ -63,7 +110,7 @@ apiRouter.post(endpoint + 'food', (req, res) => {
 });
 
 // Updade a food by id
-apiRouter.put(endpoint + 'food/:id', (req, res) => {
+apiRouter.put(endpoint + 'food/:id', checkToken, isAdmin, (req, res) => {
   let id = parseInt(req.params.id);
   knex('food')
     .where('id', id)
@@ -87,7 +134,7 @@ apiRouter.put(endpoint + 'food/:id', (req, res) => {
 });
 
 // Delete a food by id
-apiRouter.delete(endpoint + 'food/:id', (req, res) => {
+apiRouter.delete(endpoint + 'food/:id', checkToken, isAdmin, (req, res) => {
   let id = parseInt(req.params.id);
   knex('food')
     .where('id', id)
@@ -98,6 +145,63 @@ apiRouter.delete(endpoint + 'food/:id', (req, res) => {
         message: `Erro ao deletar food - ${res.status}: ${err.message}`,
       })
     );
+});
+
+// Create user
+apiRouter.post(endpoint + 'seguranca/register', (req, res) => {
+  knex('usuario')
+    .insert(
+      {
+        nome: req.body.nome,
+        login: req.body.login,
+        senha: bcrypt.hashSync(req.body.senha, 8),
+        email: req.body.email,
+      },
+      ['id']
+    )
+    .then((result) => {
+      let usuario = result[0];
+      res.status(200).json({ id: usuario.id });
+      return;
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Erro ao registrar usuario - ' + err.message,
+      });
+    });
+});
+
+apiRouter.post(endpoint + 'seguranca/login', (req, res) => {
+  knex
+    .select('*')
+    .from('usuario')
+    .where({ login: req.body.login })
+    .then((usuarios) => {
+      if (usuarios.length) {
+        let usuario = usuarios[0];
+        let checkSenha = bcrypt.compareSync(req.body.senha, usuario.senha);
+        if (checkSenha) {
+          var tokenJWT = jwt.sign({ id: usuario.id }, process.env.SECRET_KEY, {
+            expiresIn: 3600,
+          });
+          res.status(200).json({
+            id: usuario.id,
+            login: usuario.login,
+            nome: usuario.nome,
+            roles: usuario.roles,
+            token: tokenJWT,
+          });
+          return;
+        }
+      }
+
+      res.status(200).json({ message: 'Login ou senha incorretos' });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Erro ao verificar login - ' + err.message,
+      });
+    });
 });
 
 module.exports = apiRouter;
